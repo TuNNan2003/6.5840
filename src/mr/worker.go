65f8bfa3348ef8f0b8e73c41fa373a95c8182c6e
TuNNan_ubuntu <Z18149069185@outlook.com>
2023-12-16 00:04:36 +0800

    <feat> finish job for basic interaction

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,33 +1,95 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
+type KeyValueArray []KeyValue
 
-//
+func (m KeyValueArray) Len() int           { return len(m) }
+func (m KeyValueArray) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
+func (m KeyValueArray) Less(i, j int) bool { return m[i].Key < m[j].Key }
+
+type worker struct {
+	ID   int
+	task Task
+}
+
+func workerTrigger(workerID int) {
+	worker := worker{workerID, Task{}}
+	ok := call("Coordinator.WorkerRegister", &worker, nil)
+	if ok {
+		for {
+			call("Coordinator.GetTask", &worker, nil)
+			switch worker.task.tasktype {
+			//This is not a task
+			case 0:
+
+			//This is a map task
+			case 1:
+				file, err := os.Open(worker.task.filename)
+				if err != nil {
+					log.Fatalf("can't open %v", worker.task.filename)
+				}
+				content, err := ioutil.ReadAll(file)
+				if err != nil {
+					log.Fatalf("can't read %v", worker.task.filename)
+				}
+				file.Close()
+				kvResult := worker.task.mapf(worker.task.filename, string(content))
+				var nReduce int
+				ok := call("Coordinate.GetnReduce", nil, &nReduce)
+				if !ok {
+					return
+				}
+				worker.emit(&kvResult, nReduce)
+			//This is a reduce task
+			case 2:
+			//This indicates a end
+			case 3:
+			}
+		}
+	}
+}
+
+func (w *worker) emit(Result *[]KeyValue, nReduce int) {
+	switch w.task.tasktype {
+	//produce output file for map
+	case 1:
+		sort.Sort(KeyValueArray(*Result))
+		mOutFile := make([]*os.File, nReduce)
+		for i := 0; i < nReduce; i++ {
+			file, _ := os.Create(fmt.Sprintf("m-out-%d", i))
+			mOutFile[i] = file
+		}
+		for _, kv := range *Result {
+			fmt.Fprintf(mOutFile[ihash(kv.Key)%nReduce], "%v %v\n", kv.Key, kv.Value)
+		}
+	case 2:
+
+	}
+}
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
@@ -35,14 +97,20 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
-
+	var workernum int
+	ok := call("Coordinator.GetnReduce", nil, &workernum)
+	if ok {
+		for i := 1; i < workernum; i++ {
+			go workerTrigger(i)
+		}
+	} else {
+		log.Fatal("get nReduce RPC failed")
+	}
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +135,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
